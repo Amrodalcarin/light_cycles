5e25c60ddf62ac1e106e39dbbd2479ea9f4d61c9
Mauricio Manuel Cavalleri Sergent <maurimanuel92@hotmail.es>
2016-01-12 00:54:39 +0000

    Añadida IA basica y mejora de graficos en la estela

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -1,89 +1,73 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class Move : MonoBehaviour {
+    //Trail
+    private List<Vector2> puntos_trail;
+    public GameObject col;
+
     // Movement keys (customizable in Inspector)
     public KeyCode upKey;
     public KeyCode downKey;
     public KeyCode rightKey;
     public KeyCode leftKey;
+    private KeyCode last_presed;
 
     // Movement Speed
     public float speed = 16;
 
-    // Wall Prefab
-    public GameObject wallPrefab;
-
-    // Current Wall
-    Collider2D wall;
-
-    // Last Wall's End
-    Vector2 lastWallEnd;
-
     // Use this for initialization
     void Start() {
+        //Trail
+        puntos_trail = new List<Vector2>();
         // Initial Velocity
         GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
-        spawnWall();
+        last_presed = upKey;
     }
 
-    void spawnWall()
-    {
-        // Save last wall's position
-        lastWallEnd = transform.position;
-
-        // Spawn a new Lightwall
-        GameObject g = (GameObject)Instantiate(wallPrefab, transform.position, Quaternion.identity);
-        wall = g.GetComponent<Collider2D>();
-    }
-
-    void fitColliderBetween(Collider2D co, Vector2 a, Vector2 b)
-    {
-        // Calculate the Center Position
-        co.transform.position = a + (b - a) * 0.5f;
-
-        // Scale it (horizontally or vertically)
-        float dist = Vector2.Distance(a, b);
-        if (a.x != b.x)
-            co.transform.localScale = new Vector2(dist + 1, 1);
-        else
-            co.transform.localScale = new Vector2(1, dist + 1);
-    }
 
     void OnTriggerEnter2D(Collider2D co)
     {
-        // Not the current wall?
-        if (co != wall)
-        {
-            print("Player lost:" + name);
-            Destroy(gameObject);
-        }
+        print("Player lost:" + name);
+        Destroy(gameObject);
     }
 
     // Update is called once per frame
     void Update () {
+        float desiredAngle;
         // Check for key presses
-        if (Input.GetKeyDown(upKey))
+        if (Input.GetKeyDown(upKey) && last_presed != downKey)
         {
             GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
-            spawnWall();
+            desiredAngle = 0;
+            transform.rotation = Quaternion.Euler(0, 0, desiredAngle);
+            last_presed = upKey;
         }
-        else if (Input.GetKeyDown(downKey))
+        else if (Input.GetKeyDown(downKey) && last_presed != upKey)
         {
             GetComponent<Rigidbody2D>().velocity = -Vector2.up * speed;
-            spawnWall();
+            desiredAngle = 180;
+            transform.rotation = Quaternion.Euler(0, 0, desiredAngle);
+            last_presed = downKey;
         }
-        else if (Input.GetKeyDown(rightKey))
+        else if (Input.GetKeyDown(rightKey) && last_presed != leftKey)
         {
             GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
-            spawnWall();
+            desiredAngle = -90;
+            transform.rotation = Quaternion.Euler(0, 0, desiredAngle);
+            last_presed = rightKey;
         }
-        else if (Input.GetKeyDown(leftKey))
+        else if (Input.GetKeyDown(leftKey) && last_presed != rightKey)
         {
             GetComponent<Rigidbody2D>().velocity = -Vector2.right * speed;
-            spawnWall();
+            desiredAngle = 90;
+            transform.rotation = Quaternion.Euler(0, 0, desiredAngle);
+            last_presed = leftKey;
         }
-        fitColliderBetween(wall, lastWallEnd, transform.position);
+    }
+    void FixedUpdate() {
+        puntos_trail.Add(transform.position);
+        col.GetComponent<EdgeCollider2D>().points = puntos_trail.ToArray();
     }
 
 }
---
+++ Assets/TrailCollider.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class TrailCollider : MonoBehaviour {
+    private List<Vector2> puntos_trail;
+    public GameObject col;
+
+    // Use this for initialization
+    void Start () {
+        puntos_trail = new List<Vector2>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        puntos_trail.Add(transform.position);
+        col.GetComponent<EdgeCollider2D>().points = puntos_trail.ToArray();
+	}
+}
